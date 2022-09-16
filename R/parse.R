086bcc3e81fba6ac67d7edda38325b1d88bff441
JARS3N <JARS3N@users.noreply.github.com>
2022-09-16 12:19:17 -0400

    Update parse.R

--- R/parse.R
+++ R/parse.R
@@ -5,6 +5,7 @@ parse<-function (xml){
   barcode <- xpathSApply(d,path = "//InspectionDetailsItem[Name='Bar Code']//Details",xmlValue)
   if(is.null(barcode)){return(NULL)}
   tbl <- xpathSApply(d, path = "//List//InspectionDetailsItem[Name='Results']//Details",xmlValue)
+  if(is.null(tbl)){return(NULL)}
   html_tree <- try(XML::htmlTreeParse(tbl, useInternalNodes = T))
   if(is.null(html_tree)){return(NULL)}
   tds <- xpathApply(html_tree, path = "//td")
