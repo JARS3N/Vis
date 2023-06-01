c9eedfcad15c906e0aa6cf54d65bc9bb7ceb87db
JARS3N <JARS3N@users.noreply.github.com>
2023-06-01 14:03:01 -0400

    Update check_if_moved.R

--- R/check_if_moved.R
+++ R/check_if_moved.R
@@ -1,8 +1,16 @@
-check_if_moved<-function(Lot){
+check_if_moved<-function(Lot,value=F){
 # see if data has been moved to G-drive
 type <- substr(Lot,1,1)
-sub <- c("W"="XFe96","C"="XFp","B"="XFe24")[type]
+sub <- c( 
+         B = "XFe24",
+         C = "XFp",
+         W = "XFe96",
+         X = "XFe96",
+         Y = "XFe24",
+         Z = "XFp"
+         )[type]
 path <- file.path("G:","Spotting","Logging",sub)
 lotdirs <- list.dirs(path=path,recursive = F)
+ if(value==T){return(grep(Lot,lotdirs,value=T))}
 any(grepl(Lot,lotdirs))
 }
