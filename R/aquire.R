60c1b8aa0c427f91dae36ba82298f32eb5620ef5
JARS3N <JARS3N@users.noreply.github.com>
2023-06-06 15:27:50 -0400

    Update aquire.R

--- R/aquire.R
+++ R/aquire.R
@@ -3,7 +3,9 @@ aquire<- function(u){
   fls <- locate_details(u)
   n_cores<-detectCores()
   cl <- makeCluster(n_cores)
-  df <- parLapply(cl, fls, vis::parse) %>% 
+  df <- parLapply(cl, fls, function(u){
+    try(vis::parse(u))
+  }) %>% 
     dplyr::bind_rows()
   parallel::stopCluster(cl)
   df
